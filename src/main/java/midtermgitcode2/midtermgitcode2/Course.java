a24f932363f81c47c8ea2b709910aa094d7604f8
TheImpressionist-1361 <kerjames@sheridancollege.ca>
2025-10-30 09:09:55 -0400

    rev 1 local

---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Course.java
@@ -0,0 +1,18 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Course {
+
+	private int courseID;
+	private int courseName;
+
+	public void findAllCourseStudents() {
+		// TODO - implement Course.findAllCourseStudents
+		throw new UnsupportedOperationException();
+	}
+
+	public void changeCourseID() {
+		// TODO - implement Course.changeCourseID
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/midtermgitcode2/midtermgitcode2/Student.java
@@ -0,0 +1,18 @@
+package midtermgitcode2.midtermgitcode2;
+
+public class Student {
+
+	private int id;
+	private int name;
+
+	public void setName() {
+		// TODO - implement Student.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void findStudent() {
+		// TODO - implement Student.findStudent
+		throw new UnsupportedOperationException();
+	}
+
+}
